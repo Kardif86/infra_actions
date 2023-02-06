465f1cde9a42e58bb09156b422e878df3c803c3d
Павел Князев <Kardif86@rambler.ru>
2023-02-06 13:22:04 +0300

    add test with flake8

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -1,5 +1,3 @@
-from http import HTTPStatus
-
 from django.test import Client, TestCase
 from django.urls import reverse
 
@@ -11,10 +9,10 @@ class StaticPagesURLTests(TestCase):
     def test_about_url_exists_at_desired_location(self):
         """Проверка доступности страниц."""
         response = self.guest_client.get('/')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.status_code, 200)
 
         response = self.guest_client.get(reverse('/second_page/'))
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.status_code, 200)
 
     def test_page_shows_correct_content(self):
         """Проверка контента страниц."""
